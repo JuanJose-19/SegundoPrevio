a88a470d751ab298da786f3f69f6e0165377c2f9
juanjose <juanjosecg@ufps.edu.co>
2020-05-28 08:12:48 -0500

    Tercer Commit

--- src/DAO/ServicioDAO.java
+++ src/DAO/ServicioDAO.java
@@ -1,5 +1,126 @@
+
+
+
 package DAO;
 
-public class ServicioDAO {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import DTO.Servicio;
+import Modelo.Conexion;
 
+
+public class ServicioDAO {
+	
+	private Servicio servicio;
+	private List<Servicio> servicios = new ArrayList<Servicio>();
+	private Conexion con;
+	
+	public ServicioDAO(){
+		// TODO Auto-generated constructor stub
+		this.con = Conexion.getConexion();
+	}
+	
+	public void insertar(Servicio servicio){
+		String sql = "INSERT INTO servicio (id, nombre, "
+				+ "descripcion, tienda) VALUES('"+servicio.getId()+"',"
+				+ " '"+servicio.getNombre()+"', "
+				+ "'"+servicio.getDescripcion()+"', "
+				+ "'"+servicio.getTienda()+"')";
+		
+		try {
+			con.insert(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void actualizar(Servicio servicio){
+		String sql = "UPDATE servicio SET "
+				+ "nombre = '"+servicio.getNombre()+"', "
+				+ "descripcion = "+servicio.getDescripcion()+", "
+				+ "tienda = "+servicio.getTienda()+" "
+				+ "WHERE id = '"+servicio.getId()+"'";
+		try {
+			con.insert(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void eliminar(Servicio servicio){
+		String sql = "DELETE FROM servicio "
+				+ "WHERE id = '"+servicio.getId()+"'";
+		try {
+			con.insert(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+public Servicio buscar(Integer id){
+		
+		String sql = "SELECT * FROM servicio "
+				+ "WHERE id = '"+id+"'";
+		System.out.println(sql);
+		
+		servicio = null;
+		ResultSet res;
+		
+		try {
+			res = con.query(sql);
+			while(res.next()){
+				servicio = new Servicio();
+				servicio.setId(res.getInt("id"));
+        	    servicio.setNombre(res.getString("nombre"));
+        	    servicio.setDescripcion(res.getString("descripcion"));
+        	    servicio.setTienda(res.getInt("tienda"));
+				
+				
+			}
+	   	res.close();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return servicio;
+	}
+
+
+
+
+public List<Servicio> listar(){
+	String sql = "SELECT * FROM servicio ";
+	
+	
+	servicio = null;
+	ResultSet res;
+	
+	try {
+		res = con.query(sql);
+		while(res.next()){
+			servicio = new Servicio();
+			servicio.setId(res.getInt("id"));
+    	    servicio.setNombre(res.getString("nombre"));
+    	    servicio.setDescripcion(res.getString("descripcion"));
+    	    servicio.setTienda(res.getInt("tienda"));
+    	    this.servicios.add(servicio);
+		}
+   	res.close();
+		
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+	return this.servicios;
+	}
 }
---
+++ src/Pruebas/pruebas.java
@@ -0,0 +1,35 @@
+package Pruebas;
+
+import DAO.ClienteDAO;
+import DTO.Cliente;
+
+public class pruebas {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Cliente cliente = new Cliente();
+		ClienteDAO cDAO = new ClienteDAO();
+		
+		cliente.setId(100);	
+		cliente.setNombre("Juan Jose");
+		cliente.setEmail("juanjose@gmail.com");
+		cliente.setClave("1234");
+		
+		System.out.println(cDAO.listar().size());
+		
+		cDAO.insertar(cliente);
+		
+		//Cliente c2 = cDAO.buscar(100);
+		//if(c2!=null){
+			//System.out.println(c2.getNombre());
+	//	c2.setNombre("Juan");
+		//cDAO.actualizar(c2);
+			//c2 = cDAO.buscar(120);
+			//System.out.println(c2.getNombre());
+		//cDAO.eliminar(cliente);
+	}	
+
+	}
+
+}
